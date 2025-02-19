c00ae66c2e48593a8f283684089dc0280ee854e8
rHerik <kurtzgk@gmail.com>
2025-02-19 18:47:29 -0300

    fix: wrong name declaration

--- script_bash.sh
+++ script_bash.sh
@@ -6,4 +6,4 @@ systemctl enable apache2
 
 systemctl start apache2
 
-curl -X POST -d "nome-Herik Cunha" https://difusaotech.com.br/lab/aws/index.php
+curl -X POST -d "nome=Herik Cunha" https://difusaotech.com.br/lab/aws/index.php
